e50fafc2b0edd93b3431000b05323d6f70649394
Raline Silva <ralinesilva1@gmail.com>
2025-06-14 09:35:53 -0300

    registration: estruturar pastas

---
+++ PuzzleBox/Scenes/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PuzzleBox
+//
+//  Created by Raline Maria da Silva on 06/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+
+}
